678c7f9ea428cc5394a082ae06249866fcf66da1
Rohan403 <18btcse35@suiit.ac.in>
2022-02-06 12:18:43 +0530

    commit

--- Feb 2 Aggignment/script.js
+++ Feb 2 Aggignment/script.js
@@ -1,4 +1,4 @@
-var twoSum = function(nums, target) {
+/* var twoSum = function(nums, target) {
     for(var i = 0 ; i< nums.length ; i++){
         for(j=i+1 ; j<nums.length; j++){
             if(target == nums[i]+nums[j]){
@@ -6,4 +6,40 @@ var twoSum = function(nums, target) {
             }
         }
     }
-};
+}; */
+var num = 0;
+var counter = document.getElementById('counter');
+var increase = document.querySelector(".increase");
+var decrease = document.querySelector(".decrease");
+var z = null;
+increase.addEventListener("click",function(){
+    //clearInterval(dec)
+    if(z){
+        clearInterval(z)
+        z = setInterval(()=>{
+            counter.innerText ++;
+        },1000)
+    }
+    else{
+        z = setInterval(()=>{
+            counter.innerText ++;
+        },1000)
+    }
+
+})
+decrease.addEventListener("click",function(){
+    if(z){
+        clearInterval(z)
+        z = setInterval(()=>{
+            counter.innerText --;
+        },1000)
+    }
+    else{
+        z = setInterval(()=>{
+            counter.innerText --;
+        },1000)
+    }
+})
+
+
+
